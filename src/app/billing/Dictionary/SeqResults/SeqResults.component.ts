d4083c708a863f91d39e2227ea7b566e212fce83
IQ1 <IQ1@optionmatrix.com>
2019-09-16 17:02:26 +0530

    ram 16/9/2019

--- src/app/billing/Dictionary/SeqResults/SeqResults.component.ts
+++ src/app/billing/Dictionary/SeqResults/SeqResults.component.ts
@@ -19,13 +19,13 @@ export class SeqResultsComponent implements OnInit {
     }
   ngOnInit() {
     this.List = [
-      {id: "1" , AllelName: "rs4846048", Ref:"G", Variant:"A", AllelCall:"Heterozygous"  },
-      {id: "2" , AllelName: "rs1537514", Ref:"G", Variant:"C", AllelCall:"Abest" },
-      {id: "3" , AllelName: "rs868014", Ref:"A", Variant:"G", AllelCall:"Homozygous" },
-      {id: "4" , AllelName: "rs2274976", Ref:"C", Variant:"T", AllelCall:"Absent" },
-      {id: "5" , AllelName: "tvc.novel.1", Ref:"G", Variant:"C", AllelCall:"Heterozygous" },
-      {id: "6" , AllelName: "tvc.novel.2", Ref:"G", Variant:"A", AllelCall:"Homozygous" },
-      {id: "7" , AllelName: "rs1801131", Ref:"T", Variant:"G", AllelCall:"Homozygous" }
+      {id: "1" , AllelName: "rs4846048", Ref:"G", Variant:"A", Gene:"LCT",Genotype:"Homozygous ", AllelCall:"Heterozygous"  },
+      {id: "2" , AllelName: "rs1537514", Ref:"G", Variant:"C", Gene:"ADA ",Genotype:"Heterozygous", AllelCall:"Abest" },
+      {id: "3" , AllelName: "rs868014", Ref:"A", Variant:"G", Gene:"TPK ",Genotype:"Absent", AllelCall:"Homozygous" },
+      {id: "4" , AllelName: "rs2274976", Ref:"C", Variant:"T", Gene:"ADA ",Genotype:"Heterozygous", AllelCall:"Absent" },
+      {id: "5" , AllelName: "tvc.novel.1", Ref:"G", Variant:"C", Gene:"ADA ",Genotype:"Absent", AllelCall:"Heterozygous" },
+      {id: "6" , AllelName: "tvc.novel.2", Ref:"G", Variant:"A", Gene:"LCT",Genotype:"Absent", AllelCall:"Homozygous" },
+      {id: "7" , AllelName: "rs1801131", Ref:"T", Variant:"G", Gene:"TPK ",Genotype:"Homozygous ", AllelCall:"Homozygous" }
     ]
   }
 
