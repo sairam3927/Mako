1ad912b123b974dffd18b889516b65a5cccb8535
Sairam Y <ram@optionm.com>
2019-09-23 14:12:30 +0530

    today final changes

--- src/app/billing/Calculations/pregnancy-lactaion/pregnancy-lactaion.component.ts
+++ src/app/billing/Calculations/pregnancy-lactaion/pregnancy-lactaion.component.ts
@@ -13,15 +13,15 @@ export class PregnancyLactaionComponent implements OnInit {
 
   ngOnInit() {
     this.List= [
-      { id: '1', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '2', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '3', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '4', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '5', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '6', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '7', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '8', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
-      { id: '9', Nutrient:'-',  Units:'', Logic:'', Pregnancy:'', Lactation:''},
+      { id: '1', Nutrient:'APA',  Units:'L/zi', Logic:'NA', Pregnancy:'3', Lactation:'3'},
+      { id: '2', Nutrient:'Carbohydrate',  Units:'g/zi', Logic:'NA', Pregnancy:'175', Lactation:'177'},
+      { id: '3', Nutrient:'Fibre',  Units:'g/zi', Logic:'NA', Pregnancy:'28', Lactation:'129'},
+      { id: '4', Nutrient:'Thiamine',  Units:'mg/zi', Logic:'TBD', Pregnancy:'5', Lactation:'5'},
+      { id: '5', Nutrient:'Vitamin E',  Units:'mg/zi', Logic:'NA', Pregnancy:'15', Lactation:'19'},
+      { id: '6', Nutrient:'Vitamin D',  Units:'IU', Logic:'NA', Pregnancy:'400 ', Lactation:'600 '},
+      { id: '7', Nutrient:'Vitamin k',  Units:'mcg', Logic:'NA', Pregnancy:'90', Lactation:'120'},
+      { id: '8', Nutrient:'Vitamin A',  Units:'μg', Logic:'NA', Pregnancy:'700', Lactation:'900'},
+      { id: '9', Nutrient:'Vitamin B12',  Units:'mcg', Logic:'NA', Pregnancy:'2.6', Lactation:'2.8'},
     ];
   }
 
